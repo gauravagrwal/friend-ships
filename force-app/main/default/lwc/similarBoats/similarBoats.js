3857211a49bca84239624d83e94f5dcd30c3967e
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-02-04 19:39:00 +0530

    Challenge #15: Build a solution using Lightning Web Components to display Similar Boats

---
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -0,0 +1,54 @@
+import { api, LightningElement, track, wire } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
+
+export default class SimilarBoats extends NavigationMixin(LightningElement) {
+    // Private
+    currentBoat;
+    relatedBoats;
+    @track boatId;
+    error;
+
+    // public
+    @api
+    get recordId() {
+        return this.boatId;
+    }
+    set recordId(value) {
+        this.setAttribute('boatId', value);
+        this.boatId = value;
+    }
+
+    @api
+    similarBy;
+
+    // Wire custom Apex call, using the import named getSimilarBoats
+    // Populates the relatedBoats list
+    @wire(getSimilarBoats, { boatId: "$boatId", similarBy: "$similarBy" })
+    similarBoats({ error, data }) {
+        if (data)
+            this.relatedBoats = data;
+        else if (error)
+            this.error = error;
+    }
+
+    get getTitle() {
+        return 'Similar boats by ' + this.similarBy;
+    }
+    get noBoats() {
+        return !(this.relatedBoats && this.relatedBoats.length > 0);
+    }
+
+    // Navigate to record page
+    openBoatDetailPage(event) {
+        this.currentBoat = event.detail.boatId;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.currentBoat,
+                objectApiName: 'Boat__c',
+                actionName: 'view'
+            }
+        });
+    }
+}
